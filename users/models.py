8c74b16e5311b52d65de494b0f89abdc7d6054d6
Daniel Manuel <daniel.manuel@angolacables.co.ao>
2025-07-02 16:49:00 +0100

    python2

--- users/models.py
+++ users/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=80)
+    data_nascimento = models.DateField
+    nascionalidade = models.CharField(max_length=50)
+
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=150)
+    autor_livro = models.ForeignKey(Autor)
     categoria = models.CharField(max_length=50, null=True, blank=True)
     data_edition = models.DateField(auto_now=True)
     data_criacao = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.nome
+        return self.nome    
         
     
